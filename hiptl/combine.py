f2ed848ac9de263c7f17d1eb2187c31685360f13
root <root@DESKTOP-KKAMGI0.localdomain>
2021-03-16 13:13:11 -0400

    restructured so redshift-space and real-space are in the same file

--- hiptl/combine.py
+++ hiptl/combine.py
@@ -15,41 +15,42 @@ FINAL = '/lustre/cosinga/final_fields/'
 models = get_hiptl_models()
 
 # getting command-line input
-START = int(sys.argv[1])
-END = int(sys.argv[2])
-SNAPSHOT = int(sys.argv[3])
-BOX = int(sys.argv[4])
-STEP = int(sys.argv[5]) # tells if this is the 1st or 2nd step in combine process
+PREFIX = sys.argv[1]
+START = int(sys.argv[2])
+END = int(sys.argv[3])
+SNAPSHOT = int(sys.argv[4])
+BOX = int(sys.argv[5])
+STEP = int(sys.argv[6]) # tells if this is the 1st or 2nd step in combine process
 
 # opening files to write to, getting the filenames of the files we are combining
 if STEP == 0:
-    w = hp.File(BASE+'hiptl%d_%03d.%d.%d.hdf5'%(BOX, SNAPSHOT, START, END),'w')
+    w = hp.File(BASE+'%s%d_%03d.%d.%d.hdf5'%(PREFIX, BOX, SNAPSHOT, START, END),'w')
     filenos = np.arange(START, END)
-    files = ['hiptl%d_%03d.%d.hdf5'%(BOX, SNAPSHOT, i) for i in filenos]
+    files = ['%s%d_%03d.%d.hdf5'%(PREFIX, BOX, SNAPSHOT, i) for i in filenos]
 elif STEP == 1:
-    w = hp.File(FINAL+'hiptl%d_%03d.final.hdf5'%(BOX, SNAPSHOT),'w')
+    w = hp.File(FINAL+'%s%d_%03d.final.hdf5'%(PREFIX, BOX, SNAPSHOT),'w')
     filenos = np.arange(START, END, 20)
-    files = ['hiptl%d_%03d.%d.%d.hdf5'%(BOX, SNAPSHOT, i, i+20) for i in filenos]
+    files = ['%s%d_%03d.%d.%d.hdf5'%(PREFIX, BOX, SNAPSHOT, i, i+20) for i in filenos]
 else:
     raise ValueError("the STEP input must be 0 or 1")
 
-print('first file: ' + files[0]+'\n')
-print('last file: ' + files[-1]+'\n')
+print('first file: ' + files[0])
+print('last file: ' + files[-1])
 
 # sum each model's grid individually
 for m in models:
     total = np.zeros((2048, 2048, 2048), dtype=np.float32)
-    print("starting model "+m+'\n')
-    print("current total sum %.4f"%(np.sum(total)))
+    print("starting model "+m)
     for i in files:
         # it is expected that the last job will have nonexistant files
         try:
             f = hp.File(BASE+i,'r')
         except IOError:
-            print('did not find the file %s\n'%i)
+            print('did not find the file %s'%i)
         else:
             total += f[m][:]
-            print("new sum:" + str(np.sum(total))+"\n")
+            print('found file %s, adding to grid'%i)
+            print("new sum:" + str(np.sum(total)))
             f.close()
     w.create_dataset(m, data=total, compression="gzip", compression_opts=9)
 w.close()
--- hiptl/hiptl.py
+++ hiptl/hiptl.py
@@ -9,11 +9,17 @@ import h5py as hp
 import sys
 import MAS_library as masl
 from library_hicc.models import get_hiptl_models
+import redshift_space_library as rsl
 
 # reading command line inputs
 CHUNK = int(sys.argv[1])
 SNAPSHOT = int(sys.argv[2])
 BOX = int(sys.argv[3])
+if len(sys.argv) > 4:
+    AXIS = int(sys.argv[4])
+    IN_RS_SPACE = True
+else:
+    IN_RS_SPACE = False
 
 # defining needed paths
 HIPATH = '/lustre/diemer/illustris/hih2/'  # where the hiptl files are saved
@@ -25,7 +31,7 @@ hih2file = hp.File(HIPATH+"hih2_particles_%03d.%d.hdf5" %(SNAPSHOT, CHUNK), 'r')
 ptlfile = hp.File(PTLPATH+"snap_%03d.%d.hdf5" %(SNAPSHOT, CHUNK), 'r')
 
 # output files
-w = hp.File(OUTPATH+'hiptl%d_%03d.%d.hdf5' %(BOX, SNAPSHOT, CHUNK), 'w')
+w = hp.File(OUTPATH+'hiptlrs%d_%03d.%d.hdf5' %(BOX, SNAPSHOT, CHUNK), 'w')
 
 # getting author-defined constants (these COULD change but are not expected to)
 MAS = 'CIC'
@@ -34,30 +40,38 @@ models = get_hiptl_models()
 
 # getting the needed simulation-defined constants
 head = dict(ptlfile['Header'].attrs)
-LITTLE_H = head['HubbleParam']
+LITTLE_H = head['HubbleParam'] # 100 km/s/Mpc
 BOXSIZE = head['BoxSize']/1e3 #Mpc/h
+REDSHIFT = head['Redshift']
+SCALE_FACTOR = head['Time']
 
 # getting data
 mass = ptlfile['PartType0']['Masses'][:]*1e10/LITTLE_H # solar masses
 pos = ptlfile['PartType0']['CenterOfMass'][:]/1e3 # Mpc/h
+vel = ptlfile['PartType0']['Velocities'][:] * np.sqrt(SCALE_FACTOR) # km/s
 f_neut_h = hih2file['PartType0']['f_neutral_H'][:]
 
 for m in models:
     print('starting to calculate grid for %s'%m)
     field = np.zeros(GRID, dtype=np.float32)
+    # shifting the positions to redshift space
+    if IN_RS_SPACE:
+        rsl.pos_redshift_space(pos, vel, BOXSIZE, 100*LITTLE_H, REDSHIFT, AXIS)
 
     # getting the HI mass data
     h2_frac = hih2file['PartType0']['f_mol_'+m][:]
     masshi = (1-h2_frac)*f_neut_h*mass
-    print('finished calculating the hi mass, has sum %f'%np.sum(masshi))
+    print('finished calculating the hi mass, has sum %.3e'%np.sum(masshi))
 
     # f_neut_h is negative where the model isn't defined, removing negative masses.
     masshi = np.where(masshi >= 0, masshi, np.zeros(masshi.shape, dtype=np.float32))
     masshi = masshi.astype('float32')
-    print('finished removing the negative fractions, now has sum %f'%np.sum(masshi))
+    print('finished removing the negative fractions, now has sum %.3e'%np.sum(masshi))
     
     # assigning them into the field using the Mass Assignment Scheme given
     masl.MA(pos,field,BOXSIZE,MAS,masshi)
     w.create_dataset(m, data=field, compression="gzip", compression_opts=9)
-    
+
 w.close()
+
+# checking if the output file was formatted as expected
