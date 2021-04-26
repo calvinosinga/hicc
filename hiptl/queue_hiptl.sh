9ec368aeb1d32f3e7faf288b3d68833c26a88568
Calvin Osinga <cosinga@login-2.deepthought2.umd.edu>
2021-04-26 14:35:02 -0400

    updated hiptl run

--- hiptl/queue_hiptl.sh
+++ hiptl/queue_hiptl.sh
@@ -6,8 +6,8 @@ job1="${job1##* }"
 job2=$(sbatch --dependency=afterok:$job1 combine.sbatch)
 job2="${job2##* }"
 sbatch --dependency=afterok:$job2 final.sbatch
-#job1=$(sbatch hiptlrs.sbatch)
-#job1="${job1##* }"
-#job2=$(sbatch --dependency=afterok:$job1 combiners.sbatch)
-#job2="${job2##* }"
-#sbatch --dependency=afterok:$job2 finalrs.sbatch
+job1=$(sbatch hiptlrs.sbatch)
+job1="${job1##* }"
+job2=$(sbatch --dependency=afterok:$job1 combiners.sbatch)
+job2="${job2##* }"
+sbatch --dependency=afterok:$job2 finalrs.sbatch
