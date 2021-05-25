f23fa163e177fb46635500f816dbf05820bd594d
Calvin Osinga <cosinga@login-2.deepthought2.umd.edu>
2021-05-25 15:59:36 -0400

    trying to create maps

--- visualization/ht_prof.py
+++ visualization/ht_prof.py
@@ -3,14 +3,15 @@
 
 from hydrotools.interface import interface as iface_run
 
-num_processes = 1
-randomize_order = True
-sim = 'tng75' # I think this is the right identifier
-snap_idx = 99
-verbose = False
-paranoid = True
-output_path = '/lustre/cosinga/final_fields/'
-output_compression = 'gzip'
-map_get = True
-n_max_extract = None, rank_by = None,
-# extractGalaxyData([above parameters])
+#num_processes = 1
+#randomize_order = True
+#sim = 'tng75' # I think this is the right identifier
+#snap_idx = 99
+#verbose = False
+#paranoid = True
+#output_path = '/lustre/cosinga/final_fields/'
+#output_compression = 'gzip'
+# map_get = True
+# n_max_extract = None, rank_by = None,
+map_fields = ['f_mol_GD14_vol','f_mol_GK11_vol', 'f_neutral_H', 'gas_rho']
+iface_run.extractGalaxyData(sim='tng75', snap_idx=99, paranoid=True, output_path='/lustre/cosinga/final_fields/', file_suffix='_trial', output_compression='gzip', n_max_extract=1, map_get=True, map_fields=map_fields)
