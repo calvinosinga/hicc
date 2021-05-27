f55deb8f758209f6b679ea822ad299d469d80136
root <root@DESKTOP-KKAMGI0.localdomain>
2021-05-27 12:05:20 -0400

    using dif neutral fraction

--- particle/particle.py
+++ particle/particle.py
@@ -23,9 +23,11 @@ else:
 # defining needed paths
 PTLPATH = '/lustre/cosinga/tng%d/snapdir_%03d/'%(BOX, SNAPSHOT) # where the ptl files are saved
 OUTPATH = '/lustre/cosinga/hiptl_output/' # where to save the output
+HIPATH = '/lustre/diemer/illustris/hih2/'  # where the hiptl files are saved
 
 # input files
 ptlfile = hp.File(PTLPATH+"snap_%03d.%d.hdf5" %(SNAPSHOT, CHUNK), 'r')
+hih2file = hp.File(HIPATH+"hih2_particles_%03d.%d.hdf5" %(SNAPSHOT, CHUNK), 'r')
 
 # output files
 if IN_RS_SPACE:
@@ -48,26 +50,26 @@ ptltype = ['PartType'+str(i) for i in ptltype]
 nptl = head['NumPart_ThisFile']
 
 
-# getting data
-field = np.zeros(GRID, dtype=np.float32)
-for p in ptltype:
+# # getting data
+# field = np.zeros(GRID, dtype=np.float32)
+# for p in ptltype:
     
 
-    if '1' in p:
-        mass = np.ones(nptl[1]) * DMPTL
-    else:
-        mass = ptlfile[p]['Masses'][:]*1e10/LITTLE_H # solar masses
-    pos = ptlfile[p]['Coordinates'][:]/1e3 # Mpc/h
-    vel = ptlfile[p]['Velocities'][:] * np.sqrt(SCALE_FACTOR) # km/s
+#     if '1' in p:
+#         mass = np.ones(nptl[1]) * DMPTL
+#     else:
+#         mass = ptlfile[p]['Masses'][:]*1e10/LITTLE_H # solar masses
+#     pos = ptlfile[p]['Coordinates'][:]/1e3 # Mpc/h
+#     vel = ptlfile[p]['Velocities'][:] * np.sqrt(SCALE_FACTOR) # km/s
 
-    # shifting the positions to redshift space
-    if IN_RS_SPACE:
-        pos = pos_redshift_space(pos, vel, BOXSIZE, 100*LITTLE_H, REDSHIFT, AXIS)
+#     # shifting the positions to redshift space
+#     if IN_RS_SPACE:
+#         pos = pos_redshift_space(pos, vel, BOXSIZE, 100*LITTLE_H, REDSHIFT, AXIS)
 
-    # assigning them into the field using the Mass Assignment Scheme given
-    CICW(pos,field,BOXSIZE,mass)
+#     # assigning them into the field using the Mass Assignment Scheme given
+#     CICW(pos,field,BOXSIZE,mass)
 
-w.create_dataset("particles", data=field, compression="gzip", compression_opts=9)
+# w.create_dataset("particles", data=field, compression="gzip", compression_opts=9)
 
 
 # testing the different hydrogen densities
@@ -77,12 +79,16 @@ counts = np.zeros(nobins)
 p = 'PartType0'
 mass = ptlfile[p]['Masses'][:]*1e10/LITTLE_H # solar masses
 density = ptlfile[p]['Density'][:]*1e10/LITTLE_H # solar masses per kpc/h cubed
-tothyd = ptlfile[p]['GFM_Metals'][:,0] # fraction of cell that is hydrogen
+# tothyd = ptlfile[p]['GFM_Metals'][:,0] # fraction of cell that is hydrogen
+f_neut_h = hih2file['PartType0']['f_neutral_H'][:]
+# replacing the cells with negative fractions with zero
+f_neut_h = np.where(f_neut_h>=0, f_neut_h, np.zeros_like(f_neut_h))
 kpctocm = 3.086e21
 smtog = 1.989e33
 m_p=1.673e-24
 fac = 1/kpctocm**3*LITTLE_H**3*smtog/m_p
-n_h = density*tothyd*fac
+
+n_h = density*f_neut_h*fac
 pos = ptlfile[p]['Coordinates'][:]/1e3 # Mpc/h
 vel = ptlfile[p]['Velocities'][:] * np.sqrt(SCALE_FACTOR) # km/s
 print("now binning according to number density")
