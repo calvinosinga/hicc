7bc6a17d22b871a6988389679a6640e7304570fa
root <root@DESKTOP-KKAMGI0.localdomain>
2021-06-01 13:24:03 -0400

    wrote hiptlres combine

--- hiptl/res_tests/combine.py
+++ hiptl/res_tests/combine.py
@@ -23,25 +23,24 @@ STEP = int(sys.argv[6]) # tells if this is the 1st or 2nd step in combine proces
 
 # getting array of files to iterate over
 if STEP == 0:
-    w = hp.File(BASE+'%s%d_%03d.%d.%d.hdf5'%(PREFIX, BOX, SNAPSHOT, START, END),'w')
+    w = hp.File(BASE+'%s%d_%03d.%d.%d_res.hdf5'%(PREFIX, BOX, SNAPSHOT, START, END),'w')
     filenos = np.arange(START, END)
-    files = ['%s%d_%03d.%d.hdf5'%(PREFIX, BOX, SNAPSHOT, i) for i in filenos]
+    files = ['%s%d_%03d.%d_res.hdf5'%(PREFIX, BOX, SNAPSHOT, i) for i in filenos]
 elif STEP == 1:
-    w = hp.File(FINAL+'%s%d_%03d.final.hdf5'%(PREFIX, BOX, SNAPSHOT),'w')
+    w = hp.File(FINAL+'%s%d_%03d_res.final.hdf5'%(PREFIX, BOX, SNAPSHOT),'w')
     filenos = np.arange(START, END, 20)
-    files = ['%s%d_%03d.%d.%d.hdf5'%(PREFIX, BOX, SNAPSHOT, i, i+20) for i in filenos]
+    files = ['%s%d_%03d.%d.%d_res.hdf5'%(PREFIX, BOX, SNAPSHOT, i, i+20) for i in filenos]
 else:
     raise ValueError("the STEP input must be 0 or 1")
 
 print('first file: ' + files[0])
 print('last file: ' + files[-1])
-f = hp.File(BASE+files[0], 'r')
+ff = hp.File(BASE+files[0], 'r')
 keylist = list(f.keys())
-GRID = f[models[0]][:].shape
-# sum each model's grid individually
-for m in models:
-    total = np.zeros(GRID, dtype=np.float32)
-    print("starting model "+m)
+# sum each resolution's grid individually
+for k in keylist:
+    print("starting grid "+k)
+    total = np.zeros_like(ff[k][:])
     for i in files:
         # it is expected that the last job will find nonexistent files
         try:
@@ -49,9 +48,9 @@ for m in models:
         except IOError:
             print('did not find the file %s'%i)
         else:
-            total += f[m][:]
+            total += f[k][:]
             print('found file %s, adding to grid'%i)
             print("new sum:" + str(np.sum(total)))
             f.close()
-    w.create_dataset(m, data=total, compression="gzip", compression_opts=9)
+    w.create_dataset(k, data=total, compression="gzip", compression_opts=9)
 w.close()
