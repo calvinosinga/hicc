cf38bf1795d73d5a049b54d058fb1034dbbdb731
root <root@DESKTOP-KKAMGI0.localdomain>
2021-08-07 10:53:54 -0400

    added other runs

--- galaxy/assign_ptl.py
+++ galaxy/assign_ptl.py
@@ -17,10 +17,7 @@ from library_hicc.printer import Printer
 # reading command line inputs
 SNAPSHOT = int(sys.argv[1])
 BOX = int(sys.argv[2])
-RUN = sys.argv[3] # low,mid,high to test sensitivity to color definition
-RES = int(sys.argv[4]) # resolution of the grid
-AXIS = int(sys.argv[5])
-GRID = (RES,RES,RES)
+
 
 # defining needed paths
 HOME = '/lustre/cosinga/tng%d/'%BOX
@@ -43,26 +40,28 @@ mass = sub[flds[2]][:]*1e10/LITTLE_H # solar masses
 gr = sub[flds[1]][:,4] - sub[flds[1]][:,5]
 pos = sub[flds[0]][:]/1e3 * SCALE # Mpc/h, 52 MB
 vel = sub[flds[3]][:] # km/s, 52 MB
-
-resolved_mask = lhicc.is_resolved_stmass(mass[:,4])
-red_mask = lhicc.is_red_nelson(gr,mass[:,4],RUN)
-blue_mask = np.invert(red_mask)
-red_mask *= resolved_mask # removes unresolved true values
-blue_mask *= resolved_mask
-ptltypes = [0,1,4,5]
-blue_ptls = []
-red_ptls = []
-
-for i in range(len(blue_mask)):
-    for p in ptltypes:
-        ptl = il.snapshot.loadSubhalo(HOME, SNAPSHOT, i, p, fields='ParticleIDs')
-        ptl = ptl[:]
-        if blue_mask[i] == 1: # this subhalo is blue, label constituent ptls as blue
-            blue_ptls.extend(list(ptl))
-        elif red_mask[i] == 1:
-            red_ptls.extend(list(ptl))
-        
+runs = ['low','mid','high']
 w = hp.File(HOME + 'postprocessing/color_ptlID%d_%03d.hdf5'%(BOX,SNAPSHOT),'w')
-w.create_dataset("blue_ptl",data=blue_ptls, compression="gzip", compression_opts=9)
-w.create_dataset("red_ptl", data=red_ptls, compression="gzip", compression_opts=9)
+
+for RUN in runs:
+    resolved_mask = lhicc.is_resolved_stmass(mass[:,4])
+    red_mask = lhicc.is_red_nelson(gr,mass[:,4],RUN)
+    blue_mask = np.invert(red_mask)
+    red_mask *= resolved_mask # removes unresolved true values
+    blue_mask *= resolved_mask
+    ptltypes = [0,1,4,5]
+    blue_ptls = []
+    red_ptls = []
+
+    for i in range(len(blue_mask)):
+        for p in ptltypes:
+            ptl = il.snapshot.loadSubhalo(HOME, SNAPSHOT, i, p, fields='ParticleIDs')
+            ptl = ptl[:]
+            if blue_mask[i] == 1: # this subhalo is blue, label constituent ptls as blue
+                blue_ptls.extend(list(ptl))
+            elif red_mask[i] == 1:
+                red_ptls.extend(list(ptl))
+            
+    w.create_dataset("blue_ptl_%s"%RUN,data=blue_ptls, compression="gzip", compression_opts=9)
+    w.create_dataset("red_ptl_%s"%RUN, data=red_ptls, compression="gzip", compression_opts=9)
 w.close()
