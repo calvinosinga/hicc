3fa12c55e5ad81f7493dc1c93bf3b0e901f203e6
root <root@DESKTOP-KKAMGI0.localdomain>
2021-08-11 14:36:08 -0400

    trying with new hydrotools fields

--- visualization/ht_prof.py
+++ visualization/ht_prof.py
@@ -3,17 +3,15 @@
 
 from hydrotools.interface import interface as iface_run
 
-#num_processes = 1
-#randomize_order = True
-#sim = 'tng75' # I think this is the right identifier
-#snap_idx = 99
-#verbose = False
-#paranoid = True
-#output_path = '/lustre/cosinga/final_fields/'
-#output_compression = 'gzip'
-# map_get = True
-# n_max_extract = None, rank_by = None,
+
 xtf = ['gr_dust']
 shf = ['gr_normal']
-iface_run.extractGalaxyData(sim='tng75', snap_idx=99, n_max_extract=10, paranoid=True, output_path='/lustre/cosinga/HI-color/results/', 
-            file_suffix='_try', output_compression='gzip',catxt_get=True,catxt_fields=xtf, catsh_get=True, catsh_fields=shf)
+# fields from all of the particles
+ptf = ['Coordinates','Velocities','ParticleIDs']
+testmax = 10
+iface_run.extractGalaxyData(sim='tng75', snap_idx=99, n_max_extract=testmax, paranoid=True, output_path='/lustre/cosinga/HI-color/results/', 
+            file_suffix='_try', output_compression='gzip',catxt_get=True,catxt_fields=xtf, catsh_get=True, catsh_fields=shf,
+            ptlgas_get = True, ptlgas_fields=ptf,
+            ptldm_get=True, ptldm_fields=ptf,
+            ptlstr_get=True, ptlstr_fields=ptf)
+
